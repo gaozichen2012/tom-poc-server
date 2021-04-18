716e26a690e549fe177559f6e47ff954d5d283db
Tom Gao <gaozichen2012@outlook.com>
2021-04-18 14:59:50 +0800

    none

--- state_setting.h
+++ state_setting.h
@@ -1,16 +1,4 @@
-#ifndef __SETING_H__
-#define __SETING_H__
-
-#define ZPOC_UDP_SETING_TIME 4 * 5
-
-typedef struct
-{
-	struct sockaddr_in sin;
-
-	uint16 udptimer;
-	uint16 synctimer;
-} stSetingPar;
-
-unsigned short crc_fun(unsigned char *pbuf, unsigned short numc);
+#ifndef __STATE_SETTING_H
+#define __STATE_SETTING_H
 
 #endif
--- state_work.h
+++ state_work.h
@@ -1,35 +1,4 @@
-#ifndef __ZPOC_WORK_H__
-#define __ZPOC_WORK_H__
-
-//#define	ZPOC_UDP_HB_TIME	4*5
-#define ZPOC_UDP_HB_TIME 6 * 5
-
-#define ZPOC_TCP_HB_TIME 20 * 5
-
-#define ZPOC_SYNC_TIME 3600 * 3 * 5
-
-#define ZPOC_MEM_GET_MIC_MAX_TIME 10 * 5
-
-#define ZPOC_INVITE_TIMEOUT 10 * 5
-
-#define ZPOC_REGISTER_CHECK_TIMER 1 * 5
-
-#define ZPOC_ENTER_SLEEP_MAX_CNT 60 * 5 //120*5//180*5
-
-#define ZPOC_NETWORK_TYPE_CHK_TIMER 5 * 5
-
-typedef struct
-{
-	uint16 udptimer;
-	uint16 tcptimer;
-
-	uint16 synctimer;
-} stWorkPar;
-
-void work_push_evrc_ctl_msg(unsigned char ucType, unsigned char ucReason);
-int8 work_evrc_ctrl_process(void);
-int8 work_voice_test(void);
-int8 work_pcm_test(void);
-int work_create_record_send_task();
+#ifndef __STATE_WORK_H
+#define __STATE_WORK_H
 
 #endif
